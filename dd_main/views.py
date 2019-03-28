5a70b6106f491fcdd640494e645a159af355acc1
burlicconi <burlicconi@gmail.com>
2019-03-28 16:59:26 +0100

    added starting_page that actually doing something

--- dd_main/views.py
+++ dd_main/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from monsters.labels_and_desc import (race_card_desc,
+                                      race_card_title,
+                                      race_card_link,
+                                      monster_card_desc,
+                                      monster_card_title,
+                                      monster_card_link
+                                      )
+
+CARD_WITH = 100
+CARD_HEIGHT = 200
+
 
 class StartingPage(TemplateView):
     def get(self, request):
-        context = {}
+        context = {'card': {'card_h': CARD_HEIGHT, 'card_w': CARD_WITH},
+                   'race': {'desc': race_card_desc, 'title': race_card_title, 'link': race_card_link},
+                   'monsters': {'desc': monster_card_desc, 'title': monster_card_title, 'link': monster_card_link}
+                   }
         return render(request, 'starting_page.html', context)
